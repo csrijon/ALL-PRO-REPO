4fe34b2940ea14d090a185d1f49796c92764b29a
Srijon Chowdhury <csrijon92@gmail.com>
2025-07-30 18:31:08 +0530

    new portfolio using vite js

--- Rc-Port/src/components/Navbar.jsx
+++ Rc-Port/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import {React,useState} from 'react';
+
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 import './Navbar.css';
 
 const Navbar = () => {
+   const [darkmode, setdarkmode ] = useState(false);
+   const handeltoggle = () => {
+     setdarkmode(!darkmode)
+   }
+   
   return (
     <div>
       <nav className="navbar">
         <div className="navleft">Srijon</div>
         <div className="navright">
           <label className="ui-switch">
-            <input type="checkbox" />
-            <div className="slider"></div>
+            <input type="checkbox" onChange={handeltoggle} />
+            <div className='slider' >{darkmode ? <DarkModeIcon/> : <LightModeIcon/> }</div>
           </label>
         </div>
       </nav>
