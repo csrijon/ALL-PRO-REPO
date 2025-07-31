869daa118cb61dba1e0a71b75bf2353316d518be
Srijon Chowdhury <csrijon92@gmail.com>
2025-07-31 22:20:36 +0530

    change

--- Rc-Port/src/components/Navbar.jsx
+++ Rc-Port/src/components/Navbar.jsx
@@ -1,24 +1,23 @@
-import {React,useState} from 'react';
-
+import { React, useState } from 'react';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import logopng from '../assets/Logo.png'
 import './Navbar.css';
 
 const Navbar = () => {
-   const [darkmode, setdarkmode ] = useState(false);
-   const handeltoggle = () => {
-     setdarkmode(!darkmode)
-   }
-  
+  const [darkmode, setdarkmode] = useState(false);
+  const handeltoggle = () => {
+    setdarkmode(!darkmode)
+  }
+
   return (
     <div>
       <nav className="navbar">
-        <div className="navleft"><img src= {logopng}  alt="" /></div>
+        <div className="navleft"><img src={logopng} alt=""/></div>
         <div className="navright">
           <label className="ui-switch">
             <input type="checkbox" onChange={handeltoggle} />
-            <div className='slider' >{darkmode ? <DarkModeIcon className='dark' /> : <LightModeIcon className='Light' /> }</div>
+            <div className='slider' >{darkmode ? <DarkModeIcon className='dark' /> : <LightModeIcon className='Light' />}</div>
           </label>
         </div>
       </nav>
