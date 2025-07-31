aaed41413311c0edfd8b5297e5e77b0407393f1e
Srijon Chowdhury <csrijon92@gmail.com>
2025-07-31 21:49:03 +0530

    new change

--- Rc-Port/src/components/Navbar.jsx
+++ Rc-Port/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import {React,useState} from 'react';
 
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
+import logopng from '../assets/Logo.png'
 import './Navbar.css';
 
 const Navbar = () => {
@@ -13,7 +14,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar">
-        <div className="navleft">Srijon</div>
+        <div className="navleft"><img src= {logopng}  alt="" /></div>
         <div className="navright">
           <label className="ui-switch">
             <input type="checkbox" onChange={handeltoggle} />
